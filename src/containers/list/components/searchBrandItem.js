e4c78b90b7a0256b5cfbf1949f95e84f2a51a596
Jessie <iamseye@gmail.com>
2018-07-03 15:31:24 +0800

    ADD: select brand

--- src/containers/list/components/searchBrandItem.js
+++ src/containers/list/components/searchBrandItem.js
@@ -1,76 +1,54 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Select } from 'antd';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 import Icon from '../../../utils/Icon';
 import { ICONS } from '../../../utils/constants';
+import * as searchActions from '../../../actions/searchAction';
 import './searchBar.css';
 
-export default class SearchBrandItem extends Component {
+class SearchBrandItem extends Component {
   state = {
     showOptions: false,
     selectedItem: [],
   }
-  handleSelectItem = (selectedItem) => this.props.handleSelectItem(this.props.selectCategory, selectedItem);
   submitSearch = () => this.props.submitSearch();
-
-  cancelSelected() {
-    this.setState({
-      selectedItem: [],
-    }, () => this.handleSelectItem(this.state.selectedItem));
-
-    this.closeOptionBox();
-  }
 
   handleClickCategory = () => {
     this.setState({ showOptions: !this.state.showOptions });
-  }
-
-  addSelectedItem(option) {
-    this.setState({
-      selectedItem: [...this.state.selectedItem, option],
-    }, () => this.handleSelectItem(this.state.selectedItem));
-  }
-
-  removeSelectedItem(option) {
-    const selectedArray = [...this.state.selectedItem]; // make a separate copy of the array
-    const index = selectedArray.indexOf(option);
-    selectedArray.splice(index, 1);
-    this.setState(
-      { selectedItem: selectedArray },
-      () => this.handleSelectItem(this.state.selectedItem),
-    );
   }
 
   closeOptionBox = () => {
     this.setState({ showOptions: !this.state.showOptions });
   }
 
-  handleChange(value) {
-    console.log(`selected ${value}`);
+  selectBrand(value) {
+    if (!value) {
+      this.props.searchActions.setSearchBrand('');
+    } else {
+      const valueArray = value.split(' ');
+      this.props.searchActions.setSearchBrand(valueArray[1]);
+    }
   }
 
   render() {
 
+    const options = this.props.brandOptions.map((item, index) => (
+      <Select.Option value={item}> {item}</Select.Option>
+    ));
+
     const optionBox = (
       <div className="searchBarItem＿＿box">
-        <div
-          className="searchBarItem＿＿option"
-          onClick={() => this.cancelSelected()}
-        >
-          不限{this.props.name}
-        </div>
-
         <Select
           showSearch
           style={{ width: 200 }}
-          placeholder="Select a person"
+          placeholder="廠牌"
           optionFilterProp="children"
-          onChange={value => this.handleChange(value)}
-          filterOption={(input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+          onChange={value => this.selectBrand(value)}
         >
-          <Select.Option value="jack">Jack</Select.Option>
-          <Select.Option value="lucy">Lucy</Select.Option>
-          <Select.Option value="tom">Tom</Select.Option>
+          <Select.Option value="">不限廠牌</Select.Option>
+          { options }
         </Select>
 
         <div className="searchBarItem__confirm">
@@ -104,3 +82,17 @@ SearchBrandItem.propTypes = {
   handleSelectItem: PropTypes.func.isRequired,
   submitSearch: PropTypes.func.isRequired,
 };
+
+
+const mapStateToProps = state => ({
+  area: state.search.area,
+  brandOptions: state.search.brandOptions,
+  vehicleTypeOptions: state.search.vehicleTypeOptions,
+  areaOptions: state.search.areaOptions,
+});
+
+const mapDispatchToProps = dispatch => ({
+  searchActions: bindActionCreators(searchActions, dispatch),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(SearchBrandItem);
